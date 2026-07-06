3a10d92e390cd74b2f6b586674786a90367e8fc0
Akshatha B <103173313+Akshu-1106@users.noreply.github.com>
2026-07-06 09:50:23 +0530

    Refactor Addition class to use hardcoded values

--- Addition.java
+++ Addition.java
@@ -1,22 +1,11 @@
-import java.util.Scanner;
-
-public class Addition {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // Input two numbers
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
-
-        // Calculate sum
-        int sum = num1 + num2;
-
-        // Display result
-        System.out.println("Sum = " + sum);
-
-        sc.close();
+public class Addition
+    {
+        public static void main(String args[])
+        {
+            int a,b,c;
+            a=10;
+            b=20;
+            c=a+b;
+            System.out.println("Sum of 2 numbers are "+c)
+                }
     }
-}
